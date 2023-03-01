d49f5eb24ba3277522a0598f84cc2b4c00f7fb91
민선규 <gcael@naver.com>
2023-03-02 00:23:13 +0900

    20230302

---
+++ servlet/servlet/src/main/java/hello/servlet/domain/member/Member.java
@@ -0,0 +1,24 @@
+package hello.servlet.domain.member;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.servlet.annotation.WebServlet;
+
+@Getter
+@Setter
+@NoArgsConstructor
+
+public class Member {
+
+    private Long id;
+    private String username;
+    private int age;
+
+    public Member(String username, int age) {
+        this.username = username;
+        this.age = age;
+    }
+}
